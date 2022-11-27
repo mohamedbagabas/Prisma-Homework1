f4a59cf3102afe3891e93e790beb0d8cf4e5def8
mohamedbagabas <mohamedbagabas1996@gmail.com>
2022-11-27 08:38:41 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,32 @@
 import express from 'express';
 import MovieRouter from './routes/movie_routes';
+import bodyParser from "body-parser";
+import { connectDB } from "./config/db";
+
+import UserRouter from "./routes/user-routes";
+
+
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/movie', MovieRouter);
+connectDB();
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/movie", MovieRouter);
+app.use("/users", MovieRouter);
 
-app.listen(5000, () => {
-  console.log('Server is running in port 5000');
-});
+const start = (): void => {
+  try {
+    app.listen(3333, () => {
+      console.log("Server started on port 3333");
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+start();
